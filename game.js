78d768bf44b959061c7548d48d1076da29a20d0f
EonMurph <stiger.home@gmail.com>
2024-04-25 22:57:07 +0100

    initialised the grid of words

---
+++ game.js
@@ -0,0 +1,9 @@
+let gridSection;
+let grid;
+
+document.addEventListener("DOMContentLoaded", initGrid, false);
+
+function initGrid() {
+  gridSection = document.querySelector("section#grid");
+  grid = gridSection.children;
+}
